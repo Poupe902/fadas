cbbca86087e8a5cde7b67cfb0dfeb0a1073592aa
Poupe902 <stablajohnry26@hotmail.com>
2026-01-18 06:27:31 +0400

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -21,7 +21,7 @@ export const MOCK_ITEMS = [
  */
 export const INVICTUS_PAY_CONFIG = {
   API_URL: 'https://api.invictuspay.app.br/api/public/v1',
-  API_TOKEN: 'IYCoH1R6LnB5POVuv5LTKwc9uyER0IPVcY9SrSSKU5fC0E1XauTIFRAMKF50',
+  API_TOKEN: import.meta.env.VITE_INVICTUS_TOKEN || 'IYCoH1R6LnB5POVuv5LTKwc9uyER0IPVcY9SrSSKU5fC0E1XauTIFRAMKF50',
 
   OFFERS: {
     FREE_SHIPPING: 'gmvowuyoib', // Oferta de R$ 79,90
@@ -33,6 +33,6 @@ export const INVICTUS_PAY_CONFIG = {
 };
 
 export const SUPABASE_CONFIG = {
-  URL: 'https://rckqhortwgtztkcyqvik.supabase.co',
-  ANON_KEY: 'sb_publishable_C9OsSGE54AbAP2mkd9QWrw_Jyg1qYG1'
+  URL: import.meta.env.VITE_SUPABASE_URL || 'https://rckqhortwgtztkcyqvik.supabase.co',
+  ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_C9OsSGE54AbAP2mkd9QWrw_Jyg1qYG1'
 };
