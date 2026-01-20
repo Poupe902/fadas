466c918a9d78bdbb1f02d0ce6429ba12b7f5621a
Poupe902 <stablajohnry26@hotmail.com>
2026-01-19 22:34:41 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
+const utms = localStorage.getItem('utms');
+const fbp  = localStorage.getItem('fbp');
+
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
